5738fc10fc00b96e43fad6b496f49995e0e7e71c
Evgenia Kurineva <kurineva85@mail.ru>
2021-08-31 18:47:52 +0300

    feat: implement class GuessingGame

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,36 @@
+// Ваша реализация должна использовать алгоритм двоичного поиска под капотом, чтобы пройти все тесты.  По бинарному поиску мы знаем, что самое эффективное, это делить возможный диапазон пополам. Т.е. этот метод должен возвращать среднее число из доступного диапазона.Изначально диапазон задается методом setRange, а потом диапазон изменяется методами lower() и greater().
+// Методы вызываются в тестах автоматически и проверяют правильность реализации методов класса GuessingGame
+
 class GuessingGame {
-    constructor() {}
+  constructor() {
+    this.range = [];
+    this.solution = 0;
+  }
 
-    setRange(min, max) {
+  // этот метод принимает минимальное и максимальное значение диапазона чисел, в котором находится загаданное число
+  setRange(min, max) {
+    console.log('min', min, 'max', max);
+    this.range = [min, max];
+    console.log('range', this.range);
+  }
 
-    }
+  // этот метод возвращает кандидата решения (вы делаете предположение на основе диапазона и предыдущих предположений)
+  guess() {
+    return (this.solution = Math.round((this.range[0] + this.range[1]) / 2));
+    console.log('solution', this.solution);
+  }
 
-    guess() {
+  // этот метод вызывается, если предыдущий вызов guess() возвращает число (solution) больше, чем загаданное число
+  lower() {
+    this.range[1] = this.solution; //[min, this.solution];
+    console.log('lower', this.range);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  // этот метод вызывается, если предыдущий вызов guess() возвращает число (solution) меньше, чем загаданное число
+  greater() {
+    this.range[0] = this.solution; // [this.solution, max];
+    console.log('greater', this.range);
+  }
 }
 
 module.exports = GuessingGame;
